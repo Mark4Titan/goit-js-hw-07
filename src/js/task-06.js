7558b145ac5ee5f5d5c43380ffd20809c3f7f994
Mark <52928930+Mark4Titan@users.noreply.github.com>
2021-05-24 20:41:29 +0300

    дрібні правки коду

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,7 +3,7 @@ const stringLength = inputRef.getAttribute('data-length');
 
 function inputValidation(event) { 
     let inputString = inputRef.getAttribute('value');
-    if (inputRef.value.length == stringLength) {
+    if (inputRef.value.length === stringLength) {
         inputRef.classList.add('valid');
         inputRef.classList.remove('invalid');
     } else {
@@ -13,3 +13,34 @@ function inputValidation(event) {
 };
 
 inputRef.addEventListener('change', inputValidation);
+
+
+
+
+// const inputEl = document.querySelector('#validation-input');
+// const currentValue = inputEl.getAttribute('data-length');
+// console.log(currentValue)
+
+
+// let currentValueLength = parseInt(currentValue, 10);
+// const onInputBlur = event => {
+      
+//      if (inputEl.value.length ===0 ) {
+        
+//         inputEl.classList.remove('invalid');
+//         inputEl.classList.remove('valid');
+//         return
+//     } 
+    
+//     if (inputEl.value.length === currentValueLength) {
+//         inputEl.classList.add('valid');
+//         inputEl.classList.remove('invalid');
+//         return
+//     } 
+//         inputEl.classList.add('invalid');
+//         inputEl.classList.remove('valid');
+    
+      
+// };
+// inputEl.addEventListener('blur', onInputBlur);
+
