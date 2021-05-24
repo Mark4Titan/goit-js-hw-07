3ac112658893a1b60cc9bf648f520fc273645310
Mark <52928930+Mark4Titan@users.noreply.github.com>
2021-05-24 22:25:11 +0300

    останні правки перед здачею

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,48 +1,18 @@
-// const inputRef = document.querySelector('input#validation-input');
-// const stringLength = inputRef.getAttribute('data-length');
+const inputRef = document.querySelector('input#validation-input');
+const stringLength = inputRef.getAttribute('data-length');
 
-// function inputValidation(event) { 
-//     let inputString = inputRef.getAttribute('value');
-//     if (inputRef.value.length == stringLength) {
-//         inputRef.classList.add('valid');
-//         inputRef.classList.remove('invalid');
-//     } else {
-//         inputRef.classList.add('invalid');
-//         inputRef.classList.remove('valid');
-//     }
-// };
-
-// inputRef.addEventListener('change', inputValidation);
-
-
-
-const inputEl = document.querySelector('#validation-input');
-const currentValue = inputEl.getAttribute('data-length');
-console.log(currentValue)
-
-
-let currentValueLength = parseInt(currentValue, 10);
-const onInputBlur = event => {
-      
-     if (inputEl.value.length ===0 ) {
-        
-        inputEl.classList.remove('invalid');
-        inputEl.classList.remove('valid');
-        return
-    } 
-    
-    if (inputEl.value.length === currentValueLength) {
-        inputEl.classList.add('valid');
-        inputEl.classList.remove('invalid');
-        return
-    } 
-        inputEl.classList.add('invalid');
-        inputEl.classList.remove('valid');
-    
-      
+function inputValidation(event) { 
+    let inputString = inputRef.getAttribute('value');
+    if (inputRef.value.length >= stringLength) {
+        inputRef.classList.add('valid');
+        inputRef.classList.remove('invalid');
+    } else {
+        inputRef.classList.add('invalid');
+        inputRef.classList.remove('valid');
+    }
 };
-inputEl.addEventListener('blur', onInputBlur);
 
+inputRef.addEventListener('change', inputValidation);
 
 
 
